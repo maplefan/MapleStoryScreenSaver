daedad133bb2a6770693373ef3f544fdf1ee685c
dxf <812020425@qq.com>
2017-12-11 20:40:17 +0800

    first
    first commit

---
+++ MyScreenSaver/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace MyScreenSaver
+{
+    partial class From1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(148, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 0;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.BackColor = System.Drawing.Color.Black;
+            this.label.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::MyScreenSaver.Properties.Settings.Default, "MapleStory", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.label.Font = new System.Drawing.Font("宋体", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label.ForeColor = System.Drawing.Color.White;
+            this.label.Location = new System.Drawing.Point(110, 162);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(0, 64);
+            this.label.TabIndex = 1;
+            this.label.Text = global::MyScreenSaver.Properties.Settings.Default.MapleStory;
+            this.label.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 3000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // From1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::MyScreenSaver.Properties.Resources.阿里安特宫殿;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "From1";
+            this.Text = "MapleStory";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.From1_KeyPress);
+            this.MouseEnter += new System.EventHandler(this.From1_MouseEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ MyScreenSaver/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyScreenSaver
+{
+    public partial class From1 : Form
+    {
+        public From1()
+        {
+            InitializeComponent();
+            label.BackColor = Color.Transparent;
+           
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label.Text = DateTime.Now.ToString();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void From1_MouseEnter(object sender, EventArgs e)
+        {
+            //Application.Exit();  
+            //鼠标移动就退出
+        }
+
+        private void From1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Application.Exit();
+            //键盘按下就退出
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int i = DateTime.Now.Second;
+            switch (i % 20)
+            {
+                case 0: BackgroundImage = Properties.Resources.阿里安特宫殿; break;
+                case 1: BackgroundImage = Properties.Resources.埃欧雷; break;
+                case 2: BackgroundImage = Properties.Resources.冰峰雪域; break;
+                case 3: BackgroundImage = Properties.Resources.宠物公园; break;
+                case 4: BackgroundImage = Properties.Resources.地球防御本部; break;
+                case 5: BackgroundImage = Properties.Resources.东方神洲; break;
+                case 6: BackgroundImage = Properties.Resources.废弃都市; break;
+                case 7: BackgroundImage = Properties.Resources.鬼屋; break;
+                case 8: BackgroundImage = Properties.Resources.黑森林; break;
+                case 9: BackgroundImage = Properties.Resources.江户村; break;
+                case 10: BackgroundImage = Properties.Resources.里恩; break;
+                case 11: BackgroundImage = Properties.Resources.林中之城; break;
+                case 12: BackgroundImage = Properties.Resources.蚂蚁洞; break;
+                case 13: BackgroundImage = Properties.Resources.诺特勒斯码头1; break;
+                case 14: BackgroundImage = Properties.Resources.射手村; break;     
+                case 15: BackgroundImage = Properties.Resources.时间通道; break;
+                case 16: BackgroundImage = Properties.Resources.时间通道2; break;
+                case 17: BackgroundImage = Properties.Resources.水晶花园; break;
+                case 18: BackgroundImage = Properties.Resources.新叶城铁塔; break;
+                case 19: BackgroundImage = Properties.Resources.童话村; break;
+                case 20: BackgroundImage = Properties.Resources.玩具城; break;
+
+                default: break;
+            }
+
+
+
+
+        }
+
+    }
+}
